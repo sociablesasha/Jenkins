3eb19e0e5d167845d15d4ab022c697b3898aa728
yonghyunkim <sociablesasha@gmail.com>
2019-02-12 16:36:16 +0900

    Commit

---
+++ src/main/java/com/example/jenkins/A.java
@@ -0,0 +1,9 @@
+package com.example.jenkins;
+
+public class A {
+
+    public A() {
+        System.out.println("A");
+    }
+
+}
---
+++ src/main/java/com/example/jenkins/B.java
@@ -0,0 +1,9 @@
+package com.example.jenkins;
+
+public class B {
+
+    public B() {
+        System.out.println("A");
+    }
+
+}
